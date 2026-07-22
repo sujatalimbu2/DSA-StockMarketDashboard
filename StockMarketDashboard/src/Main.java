bf1821e3f25a904abee1b5eb79be59fd2726533f
sujatalimbu2 <tolilimbu2@gmail.com>
2026-07-22 19:39:20 +0545

    create initial project structure with main class and GUI frame

---
+++ StockMarketDashboard/src/Main.java
@@ -0,0 +1,13 @@
+import gui.MainFrame;
+import javax.swing.SwingUtilities;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(() -> {
+            new MainFrame();
+        });
+
+    }
+}
---
+++ StockMarketDashboard/src/gui/MainFrame.java
@@ -0,0 +1,21 @@
+package gui;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame() {
+
+        setTitle("Real-Time Financial Market Dashboard");
+
+        setSize(1000, 700);
+
+        setLocationRelativeTo(null);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setVisible(true);
+
+    }
+
+}
